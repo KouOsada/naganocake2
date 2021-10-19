cbbebd66ed14d359a780b47111a8fd49ba0eb7e9
KouOsada <kouosada.0930@gmail.com>
2021-10-19 09:39:42 +0000

    [Add]devise関係

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,53 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :publics
+
+  # public
+  devise_for :public,skip: [:passwords,], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  root 'public/homes#top'
+
+   namespace :public do
+
+    get 'public/homes/about',to: "homes#about"
+
+    resources :customers, only: [:show, :edit, :update]
+    get 'customers/unsubscribe'
+    get 'customers/withdraw'
+
+    resources :addresses, only: [:index, :edit, :update, :create, :destroy]
+
+    resources :items, only: [:index, :show]
+
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    put :cart_items, to: 'cart_items#destroy_all'
+
+    resources :orders, only: [:new, :create, :index, :show]
+    get 'orders/confirm'
+    get 'orders/thanks'
+
+  end
+
+  # admin
+  devise_for :admin, skip: [:passwords] ,controllers: {
+    registrations: "admin/registrations",
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get 'top' => 'homes#top'
+
+    resources :customers, only: [:index, :show, :edit, :update]
+
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+
+    resources :categories, only: [:create, :index, :edit, :update]
+
+    resources :orders, only: [:show, :update]
+
+    resources :order_items, only: [:update]
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
