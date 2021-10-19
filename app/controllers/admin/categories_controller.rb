1851f1d3cc671b5585b38c7752d740d31c144b2f
KouOsada <kouosada.0930@gmail.com>
2021-10-19 10:22:26 +0000

    [Add]ジャンル追加機能

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,15 +1,31 @@
 class Admin::CategoriesController < ApplicationController
 
   def create
+    @category = Category.new(category_params)
+    if
+      @category.save
+      redirect_to admin_categories_path
+    else
+      @categories = Category.new
+      render 'index'
+    end
   end
 
   def index
+    @categories = Category.all
+    @category = Category.new
   end
 
   def edit
   end
 
   def update
+  end
+  
+  private 
+  
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
