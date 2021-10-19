1cfb17be2aade2d00f446156f1a8f8e617c94f05
KouOsada <kouosada.0930@gmail.com>
2021-10-19 11:03:17 +0000

    [Add]商品登録機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,20 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if
+      @item.save
+      redirect_to admin_items_path
+    else
+      render 'new'
+    end
   end
 
   def index
+    @items = Item.all
   end
 
   def show
@@ -15,5 +24,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :sale_status, :category_id, :tax_excluded_price)
   end
 end
