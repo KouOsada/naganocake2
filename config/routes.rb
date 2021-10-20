a7600baf12cd3dc75e8d901f85d40116bc4f758a
KouOsada <kouosada.0930@gmail.com>
2021-10-20 07:59:03 +0000

    [Remove]routes.rb内の記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,77 @@
 Rails.application.routes.draw do
-#public
+
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/create'
+    get 'orders/thanks'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/unsubscribe'
+    get 'customers/withdraw'
+  end
+  namespace :public do
+    get 'cart_itemsindex/create'
+    get 'cart_itemsindex/update'
+    get 'cart_itemsindex/destroy'
+    get 'cart_itemsindex/destroy_all'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/update'
+    get 'addresses/create'
+    get 'addresses/destroy'
+  end
+  namespace :admin do
+    get 'orders/show'
+    get 'orders/update'
+  end
+  namespace :admin do
+    get 'order_items/update'
+  end
+  get 'order_items/update'
+  namespace :admin do
+    get 'items/new'
+    get 'items/create'
+    get 'items/index'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+  # public
   devise_for :public,skip: [:passwords,], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  #test
-  root to: 'public/homes#top'
-  get 'about' => 'public/homes#about'
-  get 'top' => 'admin/homes#top'
 
-  def after_sign_in_path_for(resource)
-    admin_path(resource)
-  end
+  root 'public/homes#top'
 
-  def after_sign_up_path_for(resource)
-    admin_path(resource)
-  end
+   namespace :public do
 
-  def after_sign_out_path_for(resource)
-    root_path
-  end
-
-  namespace :public do
+    get 'public/homes/about',to: "homes#about"
 
     resources :customers, only: [:show, :edit, :update]
     get 'customers/unsubscribe'
@@ -40,13 +90,14 @@ Rails.application.routes.draw do
 
   end
 
-#admin
+  # admin
   devise_for :admin, skip: [:passwords] ,controllers: {
     registrations: "admin/registrations",
     sessions: "admin/sessions"
   }
 
   namespace :admin do
+    get 'top' => 'homes#top'
 
     resources :customers, only: [:index, :show, :edit, :update]
 
@@ -57,7 +108,6 @@ Rails.application.routes.draw do
     resources :orders, only: [:show, :update]
 
     resources :order_items, only: [:update]
-
 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
